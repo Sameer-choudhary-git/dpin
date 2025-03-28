249d9621c544741c492e4894bd2959dabd848cc8
Sameer Choudhary <137263706+Sameer-choudhary-git@users.noreply.github.com>
2025-03-29 01:53:45 +0530

    Update next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ignoreDuringBuilds: true
+  images: {
+    domains: ['images.unsplash.com']
+  }
 };
 
 export default nextConfig;
